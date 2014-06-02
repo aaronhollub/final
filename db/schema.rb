75b97265d35bbdc6515b5c68430a2e2c02c12640
vagrant <vagrant@kellogg-dev-box>
2014-06-02 04:01:06 +0000

    fixed update model issues again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602035753) do
+ActiveRecord::Schema.define(version: 20140602040049) do
 
   create_table "gantts", force: true do |t|
     t.string   "gantt_url"
@@ -36,9 +36,9 @@ ActiveRecord::Schema.define(version: 20140602035753) do
 
   create_table "updates", force: true do |t|
     t.text     "summary"
-    t.integer  "project_id"
     t.integer  "user_id"
     t.datetime "created_at"
+    t.integer  "project_name"
   end
 
   create_table "users", force: true do |t|
